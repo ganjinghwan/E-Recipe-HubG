ab93f4fced23940152ee4accbcf5830003dc1395
ganjinghwan <1211110684@student.mmu.edu.my>
2024-11-15 16:49:25 +0800

    readme

--- server/server.js
+++ server/server.js
@@ -1,19 +1,27 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
-import Recipe from './models/Recipe.js';
-import mongoose from 'mongoose';
 import recipeRoutes from './routes/recipes.js';
+import path from "path";
 
 dotenv.config();
 
 const app = express();
+
+const __dirname = path.resolve();
 
 app.use(express.json()); // allows us to accepts JSON data in the req.body
 
 app.use("/api/recipesinfo", recipeRoutes);
 
 // console.log(process.env.MONGO_URI);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/client/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
